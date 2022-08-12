ad73df4fbbd6ea13e757f138272309650b9c1647
Lucas Gomes <lukmck@gmail.com>
2022-08-12 16:47:23 -0300

    Creating form to sign up new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
+  resources :users
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
