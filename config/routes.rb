8760270fc64ca390d0004490204fc9f285bb03c2
Lucas Gomes <lukmck@gmail.com>
2022-08-15 15:13:35 -0300

    Working with sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+
+  # routes to session view
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+  get '/login', to: 'sessions#new'
+
+  # routes to users view
   resources :users
+
+  # routes to static pages view
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
